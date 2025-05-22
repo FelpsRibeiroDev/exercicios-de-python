4b77076f97c6964d92195316ede6fdbab3351bb5
Felipe Ribeiro <felipe.dev2004@gmail.com>
2025-05-22 15:25:17 -0300

    Exercicio 1 - Python

---
+++ Exercício de Python 1.py
@@ -0,0 +1,14 @@
+print('Ler 2 Valores e ver se são iguais')
+print('')
+
+x = int(input('Digite um valor: '))
+y = int(input('Digite outro valor: '))
+
+if x == y:
+    print('X e Y são iguais.')
+else:
+    print('Valores diferentes')
+
+print('')
+print('===============')
+print('Fim de programa')
