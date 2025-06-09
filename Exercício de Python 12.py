b650e46ba7fef7b9dece47b77c4ac889d7243ff1
Felipe Ribeiro <felipe.dev2004@gmail.com>
2025-06-09 13:21:03 -0300

    Exercicio 12

---
+++ Exercício de Python 12.py
@@ -0,0 +1,10 @@
+age = int(input('how old are you? '))
+
+if age >= 60:
+    print('You are a old man')
+elif age >= 18:
+    print('You are adult')
+elif age >= 12:
+    print('You are a teenager')
+else:
+    print('You are a kid')
